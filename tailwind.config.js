f17ce25c3245575c8257982044bcfb6a8eca7681
gc4d <guilhermeacadorin@gmail.com>
2024-09-08 19:51:27 -0300

    projeto

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      boxShadow: {
+        custom: '-20px 20px 60px #101725, 20px -20px 60px #161f31',
+      },
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'green-light': '#9EC5AB',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+  },
+  
+  plugins: [],
+}
+
